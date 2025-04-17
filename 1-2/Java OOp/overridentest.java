b23ce0d3a10ca849daa5f5615cfbdfa8eec8f129
Saklain <aldaiansaklain@gmail.com>
2025-04-17 16:51:02 +0600

    tintin

--- 1-2/Java OOp/overridentest.java
+++ 1-2/Java OOp/overridentest.java
@@ -1,28 +1,29 @@
-class Parent {
-    static void show() {
-        System.out.println("Parent");
-    }
-}
+// class Parent {
+//     static void show() {
+//         System.out.println("Parent");
+//     }
+// }
 
-class Child extends Parent {
-    static void show() {
-        System.out.println("Child");
-    }
-}
-// This is an example of static binding, not dynamic binding (overriding).
-// In Java, static methods are not overridden, they are hidden. 
-// The method that gets called is determined at compile time based on the reference type, not the object type.
-// So, even though we create an object of Child, the static method show() of Parent is called.
-// This is an example of static binding, not dynamic binding (overriding).
-// In this case, the output will be "Parent" because static methods are not polymorphic in Java.
-// The method that gets called is determined at compile time based on the reference type, not the object type.
-// So, even though we create an object of Child, the static method show() of Parent is called.
+// class Child extends Parent {
+//     static void show() {
+//         System.out.println("Child");
+//     }
+// }
+// // This is an example of static binding, not dynamic binding (overriding).
+// // In Java, static methods are not overridden, they are hidden. 
+// // The method that gets called is determined at compile time based on the reference type, not the object type.
+// // So, even though we create an object of Child, the static method show() of Parent is called.
+// // This is an example of static binding, not dynamic binding (overriding).
+// // In this case, the output will be "Parent" because static methods are not polymorphic in Java.
+// // The method that gets called is determined at compile time based on the reference type, not the object type.
+// // So, even though we create an object of Child, the static method show() of Parent is called.
+// // This is an example of static binding, not dynamic binding (overriding).
 
 
 
-public class overridentest {
-    public static void main(String[] args){
-        Parent p = new Child();
-        p.show();  // Output: Parent — static binding!
-    }
-}
+// public class overridentest {
+//     public static void main(String[] args){
+//         Parent p = new Child();
+//         p.show();  // Output: Parent — static binding!
+//     }
+// }
